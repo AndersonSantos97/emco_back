cb5511d709b4ac94bff730944df6b307d4dedd01
anderson <andsantos979@gmail.com>
2024-10-25 10:15:36 -0600

    task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,6 +6,34 @@ import { TaskEntity } from './tasks.entity';
 
 @Injectable()
 export class TasksService {
-    
+    constructor(
+        @InjectRepository(TaskEntity)
+        private tasksRepositori: Repository<TaskEntity>
+    ){}
+
+    async all(): Promise<TaskEntity[]>{
+        return this.tasksRepositori.find();
+    }
+
+    async findId(id:number): Promise<TaskEntity | undefined>{
+        return this.tasksRepositori.findOne({
+            where:{id}
+        })
+    }
+
+    async create(data: Partial<TaskEntity>): Promise<TaskEntity>{
+        const task = this.tasksRepositori.create(data);
+        return this.tasksRepositori.save(data);
+        
+    }
+
+    async update(id: number, data: Partial<TaskEntity>): Promise<TaskEntity>{
+        await this.tasksRepositori.update(id, data)
+        return this.findId(id);
+    }
+
+    async remove(id: number): Promise<void>{
+        await this.tasksRepositori.delete(id);
+    }
 
 }
