2583ddcc6452e3658b1c39aef3d6f81175566402
anderson <andsantos979@gmail.com>
2024-10-25 12:56:34 -0600

    correccion de error en task

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TaskEntity } from './tasks.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TaskEntity])
+  ],
   providers: [TasksService],
-  controllers: [TasksController]
+  controllers: [TasksController],
+  exports: [TasksService]
 })
 export class TasksModule {}
